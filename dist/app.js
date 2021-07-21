5e378bf2a1e6d3cce9d2b0a104808b6e1907186c
Kwangwoo <bnc3049@gmail.com>
2021-07-20 20:23:19 -0600

    Add: JS

---
+++ dist/app.js
@@ -0,0 +1,46 @@
+import { VideoComponent } from "./components/page/item/videos.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent, PageItemComponent, } from "./components/page/page.js";
+import { InputDialog, } from "./components/dialog/dialog.js";
+import { MediaSectionInput } from "./components/dialog/input/media-input.js";
+import { TextSectionInput } from "./components/dialog/input/text-input.js";
+class App {
+    constructor(appRoot, dialogRoot) {
+        this.dialogRoot = dialogRoot;
+        this.page = new PageComponent(PageItemComponent);
+        this.page.attachTo(appRoot);
+        console.log("??");
+        this.bindElementToDialog("#new-image", MediaSectionInput, (input) => new ImageComponent(input.title, input.url));
+        this.bindElementToDialog("#new-video", MediaSectionInput, (input) => new VideoComponent(input.title, input.url));
+        this.bindElementToDialog("#new-video", TextSectionInput, (input) => new NoteComponent(input.title, input.body));
+        this.bindElementToDialog("#new-todo", TextSectionInput, (input) => new TodoComponent(input.title, input.body));
+        this.page.addChild(new ImageComponent("Image Title", "https://picsum.photos/800/400"));
+        this.page.addChild(new VideoComponent("Video Title", "https://youtu.be/D7cwvvA7cP0"));
+        this.page.addChild(new NoteComponent("Note Title", "Don't forget"));
+        this.page.addChild(new TodoComponent("Todo Title", "TypeScript"));
+        this.page.addChild(new ImageComponent("Image Title", "https://picsum.photos/800/400"));
+        this.page.addChild(new VideoComponent("Video Title", "https://youtu.be/D7cwvvA7cP0"));
+        this.page.addChild(new NoteComponent("Note Title", "Don't forget"));
+        this.page.addChild(new TodoComponent("Todo Title", "TypeScript"));
+    }
+    bindElementToDialog(selector, InputComponent, makeSection) {
+        const element = document.querySelector(selector);
+        element.addEventListener("click", () => {
+            const dialog = new InputDialog();
+            const input = new InputComponent();
+            dialog.addChild(input);
+            dialog.attachTo(this.dialogRoot);
+            dialog.setOnCloseListenr(() => {
+                dialog.removeFrom(this.dialogRoot);
+            });
+            dialog.setOnSubmitListenr(() => {
+                const image = makeSection(input);
+                this.page.addChild(image);
+                dialog.removeFrom(this.dialogRoot);
+            });
+        });
+    }
+}
+new App(document.querySelector(".document"), document.body);
